050c1d2295c2f2bee45433443f550e0a9c8d0d9b
Mac <redsbaniasadi1381@gmail.com>
2023-01-15 02:42:00 +0000

    add class and atribute

--- Tokenizer/simple_tokenizer.py
+++ Tokenizer/simple_tokenizer.py
@@ -2,3 +2,17 @@ import re
 from collections import Counter
 import json
 import os
+
+
+class SimpleTokenizer:
+    def __init__(self, vocab=None, unk_token="<unk>", pad_token="<pad>", bos_token="<s>", eos_token="</s>"):
+        self.unk_token = unk_token
+        self.pad_token = pad_token
+        self.bos_token = bos_token
+        self.eos_token = eos_token
+        if vocab:
+            self.token2id = vocab
+            self.id2token = {i:t for t,i in vocab.items()}
+        else:
+            self.token2id = {}
+            self.id2token = {}
