19468f638e3aed0b1b799a256143c4f14d8f06ce
Mac <redsbaniasadi1381@gmail.com>
2023-01-16 10:54:00 +0000

    add  special token

--- Tokenizer/simple_tokenizer.py
+++ Tokenizer/simple_tokenizer.py
@@ -26,3 +26,10 @@ class SimpleTokenizer:
 
     def build_vocab(self, texts, max_size=30000, min_freq=1):
             counter = Counter()
+            for t in texts:
+                counter.update(self._basic_tokenize(t))
+            most = [tok for tok, freq in counter.most_common(max_size) if freq >= min_freq]
+            specials = [self.pad_token, self.unk_token, self.bos_token, self.eos_token]
+            token_list = specials + most
+            self.token2id = {t:i for i,t in enumerate(token_list)}
+            self.id2token = {i:t for t,i in self.token2id.items()}
