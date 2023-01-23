ac01beb1be243bf7d15411946365c66be18f5650
Mac <redsbaniasadi1381@gmail.com>
2023-01-23 02:38:00 +0000

    add  another atribute

--- Tokenizer/dataset_Loader.py
+++ Tokenizer/dataset_Loader.py
@@ -3,7 +3,9 @@ from torch.utils.data import Dataset
 import csv
 
 
-
 class TextPairDataset(Dataset):
     def __init__(self, file_path, tokenizer, src_col=0, tgt_col=1, max_len=512):
         self.samples = []
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+
