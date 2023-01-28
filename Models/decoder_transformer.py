1b7f12c64b7f06592a9c1b83b9773254646c2417
Mac <redsbaniasadi1381@gmail.com>
2023-01-28 01:43:00 +0000

    adding pe

--- Models/decoder_transformer.py
+++ Models/decoder_transformer.py
@@ -6,3 +6,5 @@ import math
 class PositionalEncodingSimple(nn.Module):
     def __init__(self, d_model, max_len=512):
         super().__init__()
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
