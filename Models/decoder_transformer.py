7c6d208090d343910ef7a9d9f9c3926501948058
Mac <redsbaniasadi1381@gmail.com>
2023-01-31 09:02:00 +0000

    add another attribute

--- Models/decoder_transformer.py
+++ Models/decoder_transformer.py
@@ -17,3 +17,15 @@ class PositionalEncodingSimple(nn.Module):
         # x: batch x seq_len x d_model
         seq_len = x.size(1)
         return x + self.pe[:, :seq_len, :]
+    
+
+class DecoderOnlyTransformer(nn.Module):
+    def __init__(self, vocab_size, d_model=512, n_heads=8, n_layers=6, dim_feedforward=2048, max_len=512, dropout=0.1):
+        super().__init__()
+        self.tok_emb = nn.Embedding(vocab_size, d_model)
+        self.pos_enc = PositionalEncodingSimple(d_model, max_len=max_len)
+        decoder_layer = nn.TransformerDecoderLayer(d_model, n_heads, dim_feedforward, dropout, batch_first=True)
+        self.decoder = nn.TransformerDecoder(decoder_layer, num_layers=n_layers)
+        self.ln = nn.LayerNorm(d_model)
+        self.out = nn.Linear(d_model, vocab_size, bias=False)
+        self._d_model = d_model
