ae9f66be4a028f418e1f888ececd77b8d0f4273f
Reza Baniasadi <redsbaniasadi1381@gmail.com>
2023-02-11 01:26:00 +0000

    add class with attribute

--- train/train_engin.py
+++ train/train_engin.py
@@ -6,3 +6,16 @@ import os
 import math
 from ..utils.checkpoint_util import save_checkpoint, load_checkpoint
 from ..utils.metrics_util import compute_perplexity
+
+
+
+class TrainingEngine:
+    def __init__(self, model, optimizer, scheduler=None, device="cpu", pad_id=0, ckpt_dir="checkpoints"):
+        self.model = model.to(device)
+        self.optim = optimizer
+        self.sched = scheduler
+        self.device = device
+        self.pad_id = pad_id
+        self.criterion = nn.CrossEntropyLoss(ignore_index=pad_id)
+        self.ckpt_dir = ckpt_dir
+        os.makedirs(ckpt_dir, exist_ok=True)
